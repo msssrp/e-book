ed8390481f524ebf1739bb1d4c7c7de3b3c4b954
031-niti <earthza6263@gmail.com>
2024-01-10 09:31:56 +0700

    build navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,11 @@
+import Navbar from "@/components/Navbar";
 import { Button } from "@nextui-org/react";
+
 
 export default function Home() {
   return (
     <div>
+      <Navbar/>
       <Button>Click me</Button>
     </div>
   );
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,34 @@
 import React from "react";
+import {
+  Navbar as NextNavbar,
+  NavbarBrand,
+  NavbarContent,
+  NavbarItem,
+  Link,
+  Button,
+} from "@nextui-org/react";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <NextNavbar className="bg-[#952124]">
+      <NavbarBrand> 
+        <Link>
+          <img
+            src="https://img2.pic.in.th/pic/517c1d0e-4584-4b92-8337-781e0ad74378d4287fd729ffcb7f.jpeg"
+            alt=""
+            className="h-[3.5rem]"
+          />
+        </Link>
+      </NavbarBrand>
+      <NavbarContent justify="end">
+        <NavbarItem className="">
+          <Link href="#" className="text-white">
+            ลงทะเบียนเข้าใช้งาน
+          </Link>
+        </NavbarItem>
+      </NavbarContent>
+    </NextNavbar>
+  );
 };
 
 export default Navbar;
