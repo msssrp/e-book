093246cc18d8b91c974d1c69ef3fc9d2f8893cf8
031-niti <earthza6263@gmail.com>
2024-01-19 15:07:28 +0700

    build ui viwerPDF

--- src/app/(books)/books/[id]/page.tsx
+++ src/app/(books)/books/[id]/page.tsx
@@ -4,8 +4,43 @@ const page = ({ params }: { params: { id: string } }) => {
   const pdfUrl = "https://www.africau.edu/images/default/sample.pdf";
 
   return (
-    <div className="flex flex-col justify-center items-center">
-      <PDFViwer url={pdfUrl} />
+    <div className="flex flex-col justify-center items-center my-4 ">
+      <div className="grid grid-cols-3 ">
+        <div className="col-span1 flex flex-col items-center ">
+          <div className="rounded-md border-2 w-1/2">
+            <img
+              src="https://d2qp0siotla746.cloudfront.net/img/use-cases/ebook/template_0.jpg"
+              alt="Book-Cover"
+              className="p-3"
+            />
+          </div>
+          <div className="text-center mt-4">
+            <p>Process of Writing</p>
+          </div>
+          <div>
+            <div className="author">
+              ผู้แต่ง:
+              <p>ผู้ช่วยศาสตราจารย์อรนุช เอกพงษ์เผ่า</p>
+            </div>
+            <div className="num-read mt-4">จำนวนผู้อ่าน : 69 คน</div>
+          </div>
+        </div>
+        <div className="col-span-2 mr-4 w-[895px] space-y-3">
+          <h2 className="text-3xl font-semibold">รายการหนังสือ</h2>
+          <div className="bg-[#D9D9D9] px-5 py-3 rounded-sm breadcrumbs">
+            <ul>
+              <li>
+                <a>หน้าหลัก</a>
+              </li>
+              <li>
+                <a>รายการหนังสือทั้งหมด</a>
+              </li>
+              <li>รายละเอียดหนังสือ</li>
+            </ul>
+          </div>
+          <PDFViwer url={pdfUrl} />
+        </div>
+      </div>
     </div>
   );
 };
--- src/components/main/PDFComponents/PDFViwer.tsx
+++ src/components/main/PDFComponents/PDFViwer.tsx
@@ -21,32 +21,73 @@ const PDFViwer: React.FC<PDFUrl> = ({ url }) => {
   return (
     <>
       <div>
-        <p>
-          Page: {pageNumber} numPage : {numPages}
-        </p>
-        <div className="text-center">
+        <div className="text-center join ">
           {pageNumber == 1 ? (
-            <button className="mr-4" disabled>
-              {"disable"}
-            </button>
+            <div>
+              <button className="join-item btn " disabled>
+                {"<<"}
+              </button>
+              <button className="join-item btn " disabled>
+                {"<"}
+              </button>
+            </div>
           ) : (
-            <button className="mr-4" onClick={handlerPreviousPage}>
-              {"<<"}
-            </button>
+            <div className="text-white">
+              <button
+                className="join-item btn bg-[#952124] border-white hover:bg-[#952125e6] hover:border-[#952125e6]"
+                onClick={handlerPreviousPage}
+              >
+                <p className="text-white">{"<<"}</p>
+              </button>
+              <button
+                className=" join-item btn bg-[#952124] border-white hover:bg-[#952125e6] hover:border-[#952125e6]"
+                onClick={handlerPreviousPage}
+              >
+                <p className="text-white ">{"<"}</p>
+              </button>
+            </div>
           )}
+
           {pageNumber >= numPages! ? (
-            <button className="mr-4" disabled>
-              {"disable"}
-            </button>
+            <div>
+              <button className="join-item btn" disabled>
+                {">"}
+              </button>
+              <button className="join-item btn" disabled>
+                {">>"}
+              </button>
+            </div>
           ) : (
-            <button onClick={handlerNextPage}> {">>"} </button>
+            <div className="">
+              <button
+                className="join-item btn bg-[#952124] border-white hover:bg-[#952125e6] hover:border-[#952125e6]"
+                onClick={handlerNextPage}
+              >
+                <p className="text-white">{">"}</p>
+              </button>
+              
+              <button
+                className="join-item btn bg-[#952124] border-white hover:bg-[#952125e6] hover:border-[#952125e6]"
+                onClick={handlerNextPage}
+              >
+                <p className="text-white">{">>"}</p>
+              </button>
+            </div>
           )}
         </div>
+        <select className="select select-bordered mx-4 w-24 border-2 border-[#C8C2C2] bg-white focus:border-[#c8c2c2b0]">
+          <option disabled selected>Page:{pageNumber} / {numPages}</option>
+          <option selected>{pageNumber}</option>
+          <option selected>{pageNumber}</option>
+          <option selected>{pageNumber}</option>
+          <option selected>{pageNumber}</option>
+        </select>
       </div>
-      <Document
+      <Document className="border-2 rounded-md "
         file={pdfUrl}
         onLoadSuccess={onDocumentLoadSuccess}
-        onLoadError={(error) => console.log(error)}>
+        onLoadError={(error) => console.log(error)}
+      >
         <Page renderMode="canvas" pageNumber={pageNumber} />
       </Document>
     </>
